2bd298005787f5e678826267eb48562a02543e77
Mark <madlyinanger37@gmail.com>
2025-03-09 10:25:41 -0500

    feat: working request to azure devops for all definitions

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,35 @@
+import dotenv from 'dotenv';
 import express from 'express';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/health', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/definitions', async (req, res) => {
+  const project = '\\RCT-CI';
+  const buildDefinitionsUrl = `https://dev.azure.com/CL-Protect/RCT/_apis/build/definitions?path=${encodeURIComponent(project)}&api-version=7.1`;
+
+  const response = await fetch(buildDefinitionsUrl, {
+    headers: {
+      Authorization: `Basic ${process.env.AZURE_PAT}`,
+    },
+  });
+
+  if (!response.ok) {
+    res.status(response.status).json({ error: response.statusText });
+    return;
+  }
+
+  const data = await response.json();
+
+  res.json(data);
 });
 
 app.listen(PORT, () => {
