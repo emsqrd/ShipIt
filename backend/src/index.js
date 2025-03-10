2d7447fa6e62b056405f8d6c1097715ff4d68c70
Mark <madlyinanger37@gmail.com>
2025-03-10 12:33:48 -0500

    feat: adding builds and definitions endpoints

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const azureBaseUrl = process.env.AZURE_BASE_URL;
+const directory = encodeURIComponent(process.env.BUILD_DEFINITION_FOLDER);
 
 app.use(express.json());
 
@@ -12,27 +14,66 @@ app.get('/health', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/definitions', async (req, res) => {
-  const azureBaseUrl = process.env.AZURE_BASE_URL;
-  const ciDirectory = process.env.BUILD_DEFINITION_FOLDER;
-  const buildDefinitionsUrl = `${azureBaseUrl}/build/definitions?path=${encodeURIComponent(ciDirectory)}&api-version=7.1`;
+app.get('/releasedVersions', async (_, res) => {});
 
-  const response = await fetch(buildDefinitionsUrl, {
+app.get('/definitions', async (_, res) => {
+  const definitionsUrl = `${azureBaseUrl}/build/definitions?path=${directory}&api-version=7.1&definitionId=117`;
+  const buildsUrl = `${azureBaseUrl}/build/builds?api-version=7.1&definitions=117`;
+
+  const definitionsRes = await fetch(definitionsUrl, {
     headers: {
       Authorization: `Basic ${process.env.AZURE_PAT}`,
     },
   });
 
-  if (!response.ok) {
-    res.status(response.status).json({ error: response.statusText });
+  if (!definitionsRes.ok) {
+    res.status(definitionsRes.status).json({ error: definitionsRes.statusText });
     return;
   }
 
-  console.log(response.url);
+  const buildsRes = await fetch(buildsUrl, {
+    headers: {
+      Authorization: `Basic ${process.env.AZURE_PAT}`,
+    },
+  });
 
-  const data = await response.json();
+  if (!buildsRes.ok) {
+    res.status(buildsRes.status).json({ error: buildsRes.statusText });
+    return;
+  }
 
-  res.json(data);
+  const definition = await definitionsRes.json();
+  const buildsData = await buildsRes.json();
+  const builds = buildsData.value.filter((build) => build.buildNumber.includes('int'));
+
+  // Using Intl.DateTimeFormat to localize and format the date.
+  const dtFormatter = new Intl.DateTimeFormat('en-US', {
+    month: '2-digit',
+    day: '2-digit',
+    year: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true,
+  });
+
+  const formatLocalDate = (date) => {
+    if (!date) return null;
+    return dtFormatter.format(new Date(date)).replace(/\//g, '-').replace(',', '');
+  };
+
+  const buildData = {
+    definitionId: definition.id,
+    definitionName: definition.name,
+    builds: builds.map((build) => ({
+      id: build.id,
+      name: build.buildNumber,
+      queuedDate: formatLocalDate(build.queueTime),
+      startedDate: formatLocalDate(build.startTime),
+      finishedDate: formatLocalDate(build.finishTime),
+    })),
+  };
+
+  res.json(buildData);
 });
 
 app.listen(PORT, () => {
