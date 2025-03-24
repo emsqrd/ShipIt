e749bab2229aae1018c8e67aceb947d82014f827
Mark <madlyinanger37@gmail.com>
2025-03-23 21:59:32 -0500

    feat: simplify ReleasedVersionItem component by removing unnecessary props and click handler

--- frontend/src/components/ReleasedVersionItem/ReleasedVersionItem.tsx
+++ frontend/src/components/ReleasedVersionItem/ReleasedVersionItem.tsx
@@ -1,30 +1,14 @@
 import type { ReleasedVersion } from '../../contracts/ReleasedVersion';
 import styles from './ReleasedVersionItem.module.css';
 
-type ReleasedVersionItemProps = ReleasedVersion & {
-  onClick?: (version: ReleasedVersion) => void;
-};
-
-const ReleasedVersionItem: React.FC<ReleasedVersionItemProps> = ({
+const ReleasedVersionItem: React.FC<ReleasedVersion> = ({
   repo,
   pipelineName,
   runName,
   version,
-  onClick,
-  ...rest
 }) => {
-  const handleClick = () => {
-    onClick?.({ repo, pipelineName, runName, version, ...rest });
-  };
-
   return (
-    <div
-      role="row"
-      className={styles['version-item']}
-      onClick={onClick ? handleClick : undefined}
-      tabIndex={onClick ? 0 : undefined}
-      aria-label={`${repo} version ${version}`}
-    >
+    <div role="row" className={styles['version-item']} aria-label={`${repo} version ${version}`}>
       <span role="cell" className={styles['repo-column']}>
         <div className={styles['repo-name']}>{repo}</div>
       </span>
