460479eb0cb1ef31244a395d55e10757cc91c9c4
Mark <madlyinanger37@gmail.com>
2025-04-09 11:13:20 -0500

    fix: renaming data-testid to be consistent casing

--- frontend/src/components/ReleasedVersionItem/ReleasedVersionItem.tsx
+++ frontend/src/components/ReleasedVersionItem/ReleasedVersionItem.tsx
@@ -12,13 +12,13 @@ const ReleasedVersionItem: React.FC<ReleasedVersion> = ({
       <span data-testid="repo-column" className={styles['repo-column']}>
         <div className={styles['repo-name']}>{repo}</div>
       </span>
-      <span data-testId="pipeline-column" className={styles['pipeline-column']}>
+      <span data-testid="pipeline-column" className={styles['pipeline-column']}>
         <div className={styles['pipeline-name']}>{pipelineName}</div>
       </span>
-      <span data-testId="run-column" className={styles['run-column']}>
+      <span data-testid="run-column" className={styles['run-column']}>
         <div className={styles['run-name']}>{runName}</div>
       </span>
-      <span data-testId="version-column" className={styles['version-column']}>
+      <span data-testid="version-column" className={styles['version-column']}>
         <div className={styles['version-tag']}>{version}</div>
       </span>
     </div>
