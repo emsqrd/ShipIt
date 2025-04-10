620089826e46f130c50cacdd61c746a6356c03e3
Mark <madlyinanger37@gmail.com>
2025-04-10 18:00:35 -0500

    feat: renaming files in git after setting git config to be case sensitive

---
+++ backend/src/enums/environment.ts
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+export enum ENVIRONMENT {
+  DEV = 'dev',
+  INT = 'int',
+  PERF1 = 'perf1',
+  UAT = 'uat',
+}
---
+++ backend/src/enums/httpMethod.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+export enum HttpMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
+}
