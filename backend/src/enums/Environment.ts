adfb56d963308cdd6e70bcf0ef127a051e31e92c
Mark <madlyinanger37@gmail.com>
2025-04-10 18:55:03 -0500

    Remove duplicate PascalCase enum files

--- backend/src/enums/Environment.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-unused-vars */
-export enum ENVIRONMENT {
-  DEV = 'dev',
-  INT = 'int',
-  PERF1 = 'perf1',
-  UAT = 'uat',
-}
--- backend/src/enums/HttpMethod.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-export enum HttpMethod {
-  GET = 'GET',
-  POST = 'POST',
-  PUT = 'PUT',
-  PATCH = 'PATCH',
-  DELETE = 'DELETE',
-}
