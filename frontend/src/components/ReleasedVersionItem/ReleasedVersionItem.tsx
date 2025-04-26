de62e606bb3f60ca0cd4f549aaeee45f1a65c836
Mark <madlyinanger37@gmail.com>
2025-04-26 09:50:41 -0500

    feat: card can flip on click and render front and back sides of the card

--- frontend/src/components/ReleasedVersionItem/ReleasedVersionItem.tsx
+++ frontend/src/components/ReleasedVersionItem/ReleasedVersionItem.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import type { ReleasedVersion } from '../../contracts/ReleasedVersion';
 import styles from './ReleasedVersionItem.module.css';
 
@@ -6,9 +8,11 @@ interface ReleasedVersionItemProps {
 }
 
 const ReleasedVersionItem: React.FC<ReleasedVersionItemProps> = ({ releasedVersion }) => {
+  const [isFlipped, setFlipped] = useState(false);
+
   return (
-    <div className={styles.cardContainer}>
-      <div className={styles.card}>
+    <div className={styles.cardContainer} onClick={() => setFlipped((flipped) => !flipped)}>
+      <div className={`${styles.card} ${isFlipped ? styles.flipped : ''}`}>
         <div className={styles.front}>
           <span data-testid="repo-column" className={styles['repo-column']}>
             <label>Repo</label>
@@ -19,14 +23,14 @@ const ReleasedVersionItem: React.FC<ReleasedVersionItemProps> = ({ releasedVersi
             <div className={styles['version-tag']}>{releasedVersion.version}</div>
           </span>
         </div>
-        {/* <div className={styles['version-item-sub']}>
-        <span data-testid="pipeline-column" className={styles['pipeline-column']}>
-          <div className={styles['pipeline-name']}>{releasedVersion.pipelineName}</div>
-        </span>
-        <span data-testid="run-column" className={styles['run-column']}>
-          <div className={styles['run-name']}>{releasedVersion.runName}</div>
-        </span>
-      </div> */}
+        <div className={styles.back}>
+          <span data-testid="pipeline-column" className={styles['pipeline-column']}>
+            <div className={styles['pipeline-name']}>{releasedVersion.pipelineName}</div>
+          </span>
+          <span data-testid="run-column" className={styles['run-column']}>
+            <div className={styles['run-name']}>{releasedVersion.runName}</div>
+          </span>
+        </div>
       </div>
     </div>
   );
