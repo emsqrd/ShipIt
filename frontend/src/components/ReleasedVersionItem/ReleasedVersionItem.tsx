d003f54f8e8011bb55f16bbb29df918feba48a95
Mark <madlyinanger37@gmail.com>
2025-04-26 09:21:40 -0500

    feat: updating ui to card layout

--- frontend/src/components/ReleasedVersionItem/ReleasedVersionItem.tsx
+++ frontend/src/components/ReleasedVersionItem/ReleasedVersionItem.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import type { ReleasedVersion } from '../../contracts/ReleasedVersion';
 import styles from './ReleasedVersionItem.module.css';
 
@@ -8,39 +6,28 @@ interface ReleasedVersionItemProps {
 }
 
 const ReleasedVersionItem: React.FC<ReleasedVersionItemProps> = ({ releasedVersion }) => {
-  const [showSubItem, setShowSubItem] = useState<boolean>(false);
-
-  const handleReleasedItemClick = () => {
-    setShowSubItem(!showSubItem);
-  };
-
-  const renderSubItem = () => {
-    if (showSubItem) {
-      return (
-        <div className={styles['version-item-sub']}>
-          <span data-testid="pipeline-column" className={styles['pipeline-column']}>
-            <div className={styles['pipeline-name']}>{releasedVersion.pipelineName}</div>
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.card}>
+        <div className={styles.front}>
+          <span data-testid="repo-column" className={styles['repo-column']}>
+            <label>Repo</label>
+            <div className={styles['repo-name']}>{releasedVersion.repo}</div>
           </span>
-          <span data-testid="run-column" className={styles['run-column']}>
-            <div className={styles['run-name']}>{releasedVersion.runName}</div>
+          <span data-testid="version-column" className={styles['version-column']}>
+            <label>Version</label>
+            <div className={styles['version-tag']}>{releasedVersion.version}</div>
           </span>
         </div>
-      );
-    }
-  };
-  return (
-    <div className={styles['version-item']}>
-      <div className={styles['version-item-main']} onClick={() => handleReleasedItemClick()}>
-        <span data-testid="repo-column" className={styles['repo-column']}>
-          <label>Repo</label>
-          <div className={styles['repo-name']}>{releasedVersion.repo}</div>
+        {/* <div className={styles['version-item-sub']}>
+        <span data-testid="pipeline-column" className={styles['pipeline-column']}>
+          <div className={styles['pipeline-name']}>{releasedVersion.pipelineName}</div>
         </span>
-        <span data-testid="version-column" className={styles['version-column']}>
-          <label>Version</label>
-          <div className={styles['version-tag']}>{releasedVersion.version}</div>
+        <span data-testid="run-column" className={styles['run-column']}>
+          <div className={styles['run-name']}>{releasedVersion.runName}</div>
         </span>
+      </div> */}
       </div>
-      {renderSubItem()}
     </div>
   );
 };
