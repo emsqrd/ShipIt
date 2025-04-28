4f7dfe53992d9aa4135b395a97a6875422026611
Mark <madlyinanger37@gmail.com>
2025-04-27 21:33:37 -0500

    feat: entire card now flipping and displaying front and back

--- frontend/src/components/ReleasedVersionItem/ReleasedVersionItem.tsx
+++ frontend/src/components/ReleasedVersionItem/ReleasedVersionItem.tsx
@@ -12,40 +12,49 @@ const ReleasedVersionItem: React.FC<ReleasedVersionItemProps> = ({ releasedVersi
 
   return (
     <div className={styles.cardContainer}>
-      <div className={styles.card}>
-        <div className={`${styles.cardInner} ${isFlipped ? styles.flipped : ''}`}>
-          <div className={styles.front}>
-            <div className={styles.cardContent}>
-              <div data-testid="repo-column" className={styles.repoSection}>
-                <div className={styles.repo}>{releasedVersion.repo}</div>
-              </div>
-              <div data-testid="version-column" className={styles.versionSection}>
-                <div className={styles.version}>{releasedVersion.version}</div>
-              </div>
+      <div className={`${styles.card} ${isFlipped ? styles.flipped : ''}`}>
+        <div className={styles.front}>
+          <div className={styles.cardContent}>
+            <div data-testid="repo-column" className={styles.repoSection}>
+              <div className={styles.repo}>{releasedVersion.repo}</div>
+            </div>
+            <div data-testid="version-column" className={styles.versionSection}>
+              <div className={styles.version}>{releasedVersion.version}</div>
             </div>
           </div>
-          <div className={styles.back}>
-            <div className={styles.cardContent}>
-              <div data-testid="pipeline-column" className={styles.pipeline}>
-                <label>Pipeline</label>
-                <div className={styles.pipeline}>{releasedVersion.pipelineName}</div>
-              </div>
-              <div data-testid="run-column" className={styles.run}>
-                <label>Run</label>
-                <div className={styles.run}>{releasedVersion.runName}</div>
-              </div>
+          <div className={styles.cardFooter}>
+            <div className={styles.buttonContainer}>
+              <button
+                className={styles.detailsButton}
+                type="button"
+                onClick={() => setFlipped((f) => !f)}
+              >
+                Details
+              </button>
             </div>
           </div>
         </div>
-        <div className={styles.cardFooter}>
-          <div className={styles.buttonContainer}>
-            <button
-              className={styles.detailsButton}
-              type="button"
-              onClick={() => setFlipped((f) => !f)}
-            >
-              {isFlipped ? 'Back' : 'Details'}
-            </button>
+        <div className={styles.back}>
+          <div className={styles.cardContent}>
+            <div data-testid="pipeline-column" className={styles.pipeline}>
+              <label>Pipeline</label>
+              <div className={styles.pipeline}>{releasedVersion.pipelineName}</div>
+            </div>
+            <div data-testid="run-column" className={styles.run}>
+              <label>Run</label>
+              <div className={styles.run}>{releasedVersion.runName}</div>
+            </div>
+          </div>
+          <div className={styles.cardFooter}>
+            <div className={styles.buttonContainer}>
+              <button
+                className={styles.detailsButton}
+                type="button"
+                onClick={() => setFlipped((f) => !f)}
+              >
+                Back
+              </button>
+            </div>
           </div>
         </div>
       </div>
